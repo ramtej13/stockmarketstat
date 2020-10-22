a579a6f557d2257e79cd12ca41e1a9f96b811060
ramtej13 <ramtej.j13@gmail.com>
2020-10-22 09:31:04 +0530

    first-0.1

--- gap_up.py
+++ gap_up.py
@@ -7,19 +7,23 @@ df = df.drop(index=0, axis=0)
 df = df.sort_values("SYMBOL",ascending=True)
 
 lable = ["todaySYMBOL","OPEN","HIGH","LOW","netinvest", "PREV. CLOSE","LTP","CHNG","%CHNG","VOLUME","VALUE","52Wh","52W","365","30D",]
-df_today = pd.read_csv("data/MW-SECURITIES-IN-F&O-22-Oct-20202.csv",names=lable)
+df_today = pd.read_csv("data/today1.csv",names=lable)
 df_today = df_today.drop(index=0, axis=0)
 df_today = df_today.sort_values("todaySYMBOL",ascending=True)
 
 dictionary = {}
+invest =3000
 list = []
 for i in range(len(df["SYMBOL"].values)):
     high = float(df["HIGH"].values[i].replace(',',''))
     open = float(df_today["OPEN"].values[i].replace(',',''))
-    dictionary[str(df["SYMBOL"].values[i])] = ((open-high)/high)*100
+    dictionary[str(df["SYMBOL"].values[i])] = [((open-high)/high)*100,invest/open]
+
     # dictionary["chnage_percentage"] = (float(df["HIGH"].values[i].replace(',',''))-float(df_today["OPEN"].values[i].replace(',','')))/float(df_today["OPEN"].values[i].replace(',',''))
 
-lol = pd.DataFrame.from_dict(data=dictionary,orient='index',columns=["chnage_percentage"])
+lol = pd.DataFrame.from_dict(data=dictionary,orient='index',columns=["chnage_percentage","quantity"])
+
+
 print(lol.sort_values("chnage_percentage",ascending=False))
 
 
