d571756b64f64be817c24a67f2f6c23e327763b5
ramtej13 <ramtej.j13@gmail.com>
2020-10-23 08:49:13 +0530

    friday

--- gap_up.py
+++ gap_up.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 lable = ["SYMBOL","OPEN","HIGH","LOW","netinvest", "PREV. CLOSE","LTP","CHNG","%CHNG","VOLUME","VALUE","52Wh","52W","365","30D",]
-df = pd.read_csv("data/MW-SECURITIES-IN-F&O-22-Oct-2020.csv",names=lable)
+df = pd.read_csv("data/yesterday.csv",names=lable)
 df = df.drop(index=0, axis=0)
 df = df.sort_values("SYMBOL",ascending=True)
 
@@ -23,10 +23,9 @@ for i in range(len(df["SYMBOL"].values)):
     # dictionary["chnage_percentage"] = (float(df["HIGH"].values[i].replace(',',''))-float(df_today["OPEN"].values[i].replace(',','')))/float(df_today["OPEN"].values[i].replace(',',''))
 
 lol = pd.DataFrame.from_dict(data=dictionary,orient='index',columns=["chnage_percentage","quantity"])
+lol = lol.sort_values("chnage_percentage",ascending=False)
 
-
-print(lol.sort_values("chnage_percentage",ascending=False))
-
+print(lol.head())
 
 
 
